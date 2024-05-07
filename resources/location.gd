3e7f79cf83c5436e8c0026a82a12f05bf2285a17
Max <lhmax55@gmail.com>
2024-05-07 18:36:28 -0400

    Disable buttons and map if locked

--- resources/location.gd
+++ resources/location.gd
@@ -4,4 +4,6 @@ extends Resource
 @export var identifier : String = "spider_cavern"
 @export var display_name : String = "Spider Cavern"
 @export var tilemap : PackedScene 
+@export var completed : bool = false
+@export var unlocked : bool = false
 
