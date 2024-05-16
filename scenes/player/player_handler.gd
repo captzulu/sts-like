d490661977bb08a0ff280e4a234d4d9735762de5
Max <lhmax55@gmail.com>
2024-05-16 13:46:32 -0400

    Utiliser le player.stats au lieu de character dans player_handler

--- scenes/player/player_handler.gd
+++ scenes/player/player_handler.gd
@@ -6,7 +6,6 @@ const HAND_DISCARD_INTERVAL : float = 0.25
 
 @export var hand : Hand
 
-var character : CharacterStats
 var player : Player 
 var hand_being_discarded : bool = false
 var enemy_killed_this_turn : int = 0
@@ -15,11 +14,10 @@ func _ready() -> void:
 	Events.player_turn_ended.connect(end_turn)
 	Events.card_played.connect(_on_card_played)
 
-func start_battle(char_stats : CharacterStats) -> void:
-	character = char_stats
-	character.draw_pile = character.deck.duplicate(true)
-	character.draw_pile.shuffle()
-	character.discard = CardPile.new()
+func start_battle() -> void:
+	player.stats.draw_pile = player.stats.deck.duplicate(true)
+	player.stats.draw_pile.shuffle()
+	player.stats.discard = CardPile.new()
 	start_turn()
 	
 func start_turn() -> void:
@@ -27,9 +25,9 @@ func start_turn() -> void:
 		player.heal_from_combos(enemy_killed_this_turn)
 	enemy_killed_this_turn = 0
 	Events.player_turn_started.emit()
-	character.block = 0
-	character.reset_mana()
-	draw_cards(character.cards_per_turn)
+	player.stats.block = 0
+	player.stats.reset_mana()
+	draw_cards(player.stats.cards_per_turn)
 	
 func end_turn() -> void:
 	hand.disable_hand()
@@ -37,7 +35,7 @@ func end_turn() -> void:
 
 func draw_card() -> void:
 	reshuffle_deck_from_discard()
-	var card : Card = character.draw_pile.draw_card()
+	var card : Card = player.stats.draw_pile.draw_card()
 	hand.add_card(card)
 	reshuffle_deck_from_discard()
 	
@@ -55,7 +53,7 @@ func discard_cards() -> void:
 	hand_being_discarded = true
 	var tween := create_tween()
 	for card_ui in hand.get_children():
-		tween.tween_callback(character.discard.add_card.bind(card_ui.card))
+		tween.tween_callback(player.stats.discard.add_card.bind(card_ui.card))
 		tween.tween_callback(hand.discard_card.bind(card_ui))
 		tween.tween_interval(HAND_DISCARD_INTERVAL)
 	
@@ -66,13 +64,13 @@ func discard_cards() -> void:
 	)
 
 func reshuffle_deck_from_discard() -> void:
-	if not character.draw_pile.empty():
+	if not player.stats.draw_pile.empty():
 		return
 	
-	while not character.discard.empty():
-		character.draw_pile.add_card(character.discard.draw_card())
+	while not player.stats.discard.empty():
+		player.stats.draw_pile.add_card(player.stats.discard.draw_card())
 	
-	character.draw_pile.shuffle()
+	player.stats.draw_pile.shuffle()
 	
 func _on_card_played(card : Card) -> void:
-	character.discard.add_card(card)
+	player.stats.discard.add_card(card)
