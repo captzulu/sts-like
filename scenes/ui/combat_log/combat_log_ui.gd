b92461e11aff38258ebdd26385c86cdcce124e39
Max <lhmax55@gmail.com>
2024-05-31 21:14:02 -0400

    Allow to collapse the combat log

--- scenes/ui/combat_log/combat_log_ui.gd
+++ scenes/ui/combat_log/combat_log_ui.gd
@@ -3,11 +3,18 @@ extends PanelContainer
 
 const DEFAULT_STYLEBOX := preload("res://scenes/ui/combat_log/combat_log.tres")
 const HOVER_STYLEBOX := preload("res://scenes/ui/combat_log/combat_log_hover.tres")
+const FULL_HEIGHT : int = 120
+const COLLAPSED_HEIGHT : int = 40
+
+@onready var expand_button = %ExpandButton
+@onready var is_collapsed : bool = %ExpandButton.is_pressed()
 
 func _ready() -> void:
 	Events.combat_log_updated.connect(on_log_updated)
 	mouse_entered.connect(on_mouse_entered)
 	mouse_exited.connect(on_mouse_exited)
+	%ExpandButton.pressed.connect(on_expand_click)
+	resize_window()
 	
 func on_log_updated(line_added : String) -> void:
 	%CombatLogText.text += "[p]" + line_added + "[/p]"
@@ -20,3 +27,16 @@ func on_mouse_exited() -> void:
 
 func change_style_box(_style_box: Resource) -> void:
 	set("theme_override_styles/panel", _style_box)
+
+func on_expand_click() -> void:
+	is_collapsed = ! is_collapsed
+	resize_window()
+
+func resize_window() -> void:
+	print("is_collapsed : " + str(is_collapsed))
+	if is_collapsed:
+		self.size.y = FULL_HEIGHT
+	else:
+		self.size.y = COLLAPSED_HEIGHT
+	print("size y : " + str(self.size.y))
+	
