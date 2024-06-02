30c4aec06cc2dcfc53a07e32886f42d182f434ab
LudovicLH <bobbylh@live.fr>
2024-06-01 20:27:29 -0400

    added surgical strike and measured strike

---
+++ characters/warrior/cards/scripts/warrior_surgical_strike.gd
@@ -0,0 +1,11 @@
+extends Card
+
+@export var effects : Dictionary = {
+	"damage" : 20
+}
+
+func apply_effects(targets : Array[Node], player : Player) -> void:
+	var damage_effect : DamageEffect = DamageEffect.new(effects["damage"], sound, player)
+	if targets[0].stats.health < effects["damage"]:
+		damage_effect.damage = targets[0].stats.health
+	damage_effect.execute(targets)
