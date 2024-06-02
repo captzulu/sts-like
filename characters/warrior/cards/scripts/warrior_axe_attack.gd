2857852acc36b1484ddb1f24fc6e731dcefceadd
Max <lhmax55@gmail.com>
2024-06-01 22:30:24 -0400

    remove print

--- characters/warrior/cards/scripts/warrior_axe_attack.gd
+++ characters/warrior/cards/scripts/warrior_axe_attack.gd
@@ -5,6 +5,5 @@ extends Card
 }
 
 func apply_effects(targets : Array[Node], player : Player) -> void:
-	print(targets[0])
 	var damage_effect : DamageEffect = DamageEffect.new(effects["damage"], sound, player)
 	damage_effect.execute(targets)
