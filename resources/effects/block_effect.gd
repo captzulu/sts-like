4921405922763d05798c0919ddc2420fe17e2568
Max <lhmax55@gmail.com>
2024-09-24 22:50:06 -0400

    Move the effects into ressources

--- resources/effects/block_effect.gd
+++ resources/effects/block_effect.gd
@@ -8,7 +8,7 @@ func _init(amount_in : int,  sound_in : AudioStream, originator_in : Node = null
 	sound = sound_in
 	originator = originator_in
 
-func execute(targets: Array[Node]) -> void:
+func execute(targets: Array) -> void:
 	for target in targets:
 		if not target is Enemy and not target is Player:
 			continue
