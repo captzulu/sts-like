16e541105ab0e328adc2797a57c08bf757178f22
Max <lhmax55@gmail.com>
2024-09-28 12:21:26 -0400

    Add new card that gives back energy and does more damage on restrained target

---
+++ characters/warrior/cards/scripts/warrior_ram_through.gd
@@ -0,0 +1,13 @@
+extends Card
+
+@export var effects : Dictionary = {
+	"damage" : 5
+}
+
+func apply_effects(targets : Array[Node], player : Player) -> void:
+	var damage : int = effects["damage"]
+	if targets[0].stats.get_status_count(Restrained) > 0:	
+		damage = damage * 2
+		player.stats.mana += 1
+	var damage_effect : DamageEffect = DamageEffect.new(damage, sound, player)
+	damage_effect.execute(targets)
