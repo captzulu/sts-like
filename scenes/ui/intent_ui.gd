cd5dbec72ff1dfa2c9cd6ba4507213fdaf74c3ec
Max <lhmax55@gmail.com>
2024-10-03 21:35:37 -0400

    Ajust textbox size depending on text length

--- scenes/ui/intent_ui.gd
+++ scenes/ui/intent_ui.gd
@@ -19,8 +19,20 @@ func update_intent(intents : Array[Intent]) -> void:
 		icon.texture = intent.icon
 		icon.visible = icon.texture != null
 		number.text = str(intent.number)
+		adjust_number_min_width(number.text, number)
 		number.visible = intent.number.length() > 0
 		intent_ui.show()
 		i += 1
 	
 	show()
+	
+func adjust_number_min_width(number_text : String, number_label : RichTextLabel) -> void:
+	var regex = RegEx.new()
+	regex.compile("\\[.*?\\]")
+	number_text = regex.sub(number_text, "", true)
+	if number_text.length() == 3:
+		number_label.set_custom_minimum_size(Vector2(14,8))
+	elif number_text.length() == 2:
+		number_label.set_custom_minimum_size(Vector2(10,8))
+	elif number_text.length() == 1:
+		number_label.set_custom_minimum_size(Vector2(5,8))
