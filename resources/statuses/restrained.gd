588407ae939bc61057d2b8e62b02c9965b605678
Max <lhmax55@gmail.com>
2024-10-03 21:51:19 -0400

    Remove restrained separately for player and enemies

--- resources/statuses/restrained.gd
+++ resources/statuses/restrained.gd
@@ -12,10 +12,17 @@ static func get_identifier() -> String:
 func _init(amount : int) -> void:
 	stacks = amount
 	icon = preload(icon_path)
-	Events.player_turn_ended.connect(_on_player_turn_ended)
+	if is_on is Player:
+		Events.player_turn_ended.connect(_on_player_turn_ended)
+	
+	if is_on is Enemy:
+		Events.enemy_turn_ended.connect(_on_enemy_turn_ended)
 
 func generate_tooltip() -> String:
 	return "Reduces damage by [color=\"" + Globals.TOOLTIP_HIGHLIGHT_TEXT_COLOR + "\"]33%[/color] for the next [color=\"" + Globals.TOOLTIP_HIGHLIGHT_TEXT_COLOR + "\"]%s[/color] turns" % str(stacks)
-		
+
 func _on_player_turn_ended() -> void:
+	decrease_stacks(1)
+
+func _on_enemy_turn_ended() -> void:
 	decrease_stacks(1)
