79da25509ff23eb403812ff1fbddee391cbd1a4e
Max <lhmax55@gmail.com>
2025-02-20 22:01:49 -0500

    Rename get_status_count and add get_status

--- characters/warrior/cards/scripts/warrior_noxious_gas.gd
+++ characters/warrior/cards/scripts/warrior_noxious_gas.gd
@@ -2,18 +2,20 @@ extends Card
 
 @export var effects : Dictionary = {}
 
-func apply_effects(targets : Array[Node], player : Player) -> void:
+func apply_effects(targets : Array[Node], _player : Player) -> void:
 	var enemy_most_poisoned : Enemy = null
 	for enemy in targets:
-		if not enemy_most_poisoned or enemy_most_poisoned.stats.get_status_count(Poison) < enemy.stats.get_status_count(Poison):
+		if not enemy_most_poisoned or enemy_most_poisoned.stats.get_status_stacks(Poison) < enemy.stats.get_status_stacks(Poison):
 			enemy_most_poisoned = enemy
 
 	var original_targets : Array = targets.duplicate()
-	if (enemy_most_poisoned.stats.get_status_count(Poison) > 0):
+	if (enemy_most_poisoned.stats.get_status_stacks(Poison) > 0):
 		targets.erase(enemy_most_poisoned)
 		for enemy in targets:
-			var poison : Poison = Poison.new(enemy_most_poisoned.stats.get_status_count(Poison))
+			var poison : Poison = Poison.new(enemy_most_poisoned.stats.get_status_stacks(Poison))
 			poison.add_to(enemy)
 		
-	for enemy in original_targets:
-		enemy.take_damage(enemy.stats.get_status_count(Poison))
+	for enemy : Enemy in original_targets:
+		var poison : Poison = enemy.stats.get_status(Poison)
+		enemy.take_damage(poison.stacks, true)
+		poison.decrease_stacks(1)
--- resources/stats.gd
+++ resources/stats.gd
@@ -29,11 +29,17 @@ func remove_status(identifier : String) -> void:
 		statuses_dict.erase(identifier)
 	stats_changed.emit()
 
-func get_status_count(status) -> int:
+func get_status_stacks(status) -> int:
 	if statuses_dict.has(status.identifier):
 		return statuses_dict[status.identifier].stacks
 	else:
 		return 0
+
+func get_status(status):
+	if statuses_dict.has(status.identifier):
+		return statuses_dict[status.identifier]
+	else:
+		return null
 	
 func take_damage(damage : int, ignore_block : bool = false) -> void:
 	if damage <= 0:
@@ -42,10 +48,10 @@ func take_damage(damage : int, ignore_block : bool = false) -> void:
 	if not ignore_block:
 		damage = clampi(damage - block, 0, damage)
 		self.block = clampi(block - initial_damage, 0, block)
-	if get_status_count(Undying) > 0 and self.health - damage < 1:
+	if get_status_stacks(Undying) > 0 and self.health - damage < 1:
 		damage = clampi(damage, 0, self.health - 1)
 		statuses_dict[Undying.identifier].decrease_stacks(1)
-	if get_status_count(Enrage) > 0:
+	if get_status_stacks(Enrage) > 0:
 		statuses_dict[Enrage.identifier].is_being_hit()
 	self.health -= damage
 
@@ -54,10 +60,10 @@ func heal(amount : int) -> void:
 
 func current_damage_modifier() -> float:
 	var damage_modifier : int = 0
-	if get_status_count(Restrained) > 0:
+	if get_status_stacks(Restrained) > 0:
 		damage_modifier += Restrained.damage_multiplier
 	
-	if get_status_count(Enrage) > 0:
+	if get_status_stacks(Enrage) > 0:
 		damage_modifier += statuses_dict[Enrage.identifier].get_damage_multiplier()
 	
 	return damage_modifier / 100.0 + 1
