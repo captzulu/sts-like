91317957befe86080a5f21a391c34d4dc4df4c54
Max <lhmax55@gmail.com>
2025-03-06 18:55:13 -0500

    Add damage spike as an effect

--- characters/warrior/cards/scripts/warrior_spiky_truncheon.gd
+++ characters/warrior/cards/scripts/warrior_spiky_truncheon.gd
@@ -2,11 +2,12 @@ extends Card
 
 @export var effects : Dictionary = {
 	"damage" : 5,
-	"spikes" : 1
+	"damage_spike" : 2,
+	"spikes" : 1,
 }
 
 func apply_effects(targets : Array[Node], player : Player) -> void:
 	Spike.new(effects["spikes"]).add_to(player)
-	var spike_damage : int = player.get_spikes() * 2
+	var spike_damage : int = player.get_spikes() * effects["damage_spike"]
 	var damage_effect : DamageEffect = DamageEffect.new(effects["damage"] + spike_damage, sound, player)
 	damage_effect.execute(targets)
