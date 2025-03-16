03d8e746ed64473324588ac643804b67853403a7
Max <lhmax55@gmail.com>
2025-03-16 16:24:52 -0400

    unselect non aiming cards when under the hand like aiming cards

--- scenes/card_ui/card_states/card_dragging_state.gd
+++ scenes/card_ui/card_states/card_dragging_state.gd
@@ -1,6 +1,7 @@
 extends CardState
 
 const DRAG_MINIMUM_THRESHOLD : float = 0.05
+const MOUSE_Y_SNAPBACK_THRESHOLD := 330
 
 var minimum_drag_time_elapsed : bool = false
 
@@ -24,6 +25,7 @@ func on_input(event: InputEvent) -> void:
 	var mouse_motion : bool = event is InputEventMouseMotion
 	var cancel : bool = event.is_action_pressed("right_mouse")
 	var confirm : bool = event.is_action_released("left_mouse") or event.is_action_pressed("left_mouse")
+	var mouse_at_bottom := card_ui.get_global_mouse_position().y > MOUSE_Y_SNAPBACK_THRESHOLD
 	
 	if single_targeted and mouse_motion and card_ui.targets.size() > 0:
 		transition_requested.emit(self, CardState.State.AIMING)
@@ -32,7 +34,7 @@ func on_input(event: InputEvent) -> void:
 	if mouse_motion:
 		card_ui.global_position = card_ui.get_global_mouse_position() - card_ui.pivot_offset
 		
-	if cancel:
+	if cancel or mouse_at_bottom:
 		transition_requested.emit(self, CardState.State.BASE)
 	elif minimum_drag_time_elapsed and confirm:
 		get_viewport().set_input_as_handled()
